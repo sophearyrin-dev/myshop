fcd78ffef160c8cdb5edbcedc595e42e7f7dfbe0
sophearyrin1 <srin@miu.edu>
2024-07-12 16:10:25 -0500

    class

--- src/main.ts
+++ src/main.ts
@@ -16,16 +16,33 @@ let log2 = (message: any) => {
 }
 
 //interface
-interface Point {
-  x: number,
-  y: number
+// interface Point {
+//   x: number,
+//   y: number
+// }
+
+// let drawPoint = (point : Point) => {
+
+// }
+
+// drawPoint({x:10, y:20});
+
+//Class
+class Point {
+  x: number;
+  y: number;
+
+  draw() {
+    console.log(`X: ${this.x}, Y: ${this.y}`);
+  }
+
+  getDistance(another : Point){
+    let dx = this.x - another.x;
+    let dy = this.y - another.y;
+    return Math.sqrt(dx*dx + dy*dy);
+  }
+
 }
-
-let drawPoint = (point : Point) => {
-
-}
-
-drawPoint({x:10, y:20});
 
 
 
