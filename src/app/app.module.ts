6ceeaafc002bb5a1d33a446ea942fc64131a949a
sophearyrin1 <srin@miu.edu>
2024-07-21 21:42:43 -0500

    created component with command

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from '../courses.component';
@@ -14,7 +14,7 @@ import { CoursesComponent } from '../courses.component';
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([])
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
