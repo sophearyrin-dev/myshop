abbbcd0570ebf77f199a58241de7b6f0bf9e3816
sophearyrin1 <srin@miu.edu>
2024-07-22 21:51:51 -0500

    directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CoursesComponent } from '../courses.component';
+import { CoursesComponent } from './courses.component';
 
 @NgModule({
   declarations: [
-    CoursesComponent
+    CoursesComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot([]),
     HttpClientModule
   ],
   providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/courses.component.ts
+++ src/courses.component.ts
@@ -4,9 +4,17 @@ import { Component } from "@angular/core";
 //Decorator Pattern
 @Component({
     selector: 'courses',
-    template: '<h2>{{ title }}</h2>'
+    template: `
+        <h2>{{ title }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course}}
+            </li>
+        </ul>
+    `
 })
 
 export class CoursesComponent {
-    title ="Course Title"
+    title ="Course Title";
+    courses = ["Course 1", "Course 2", "Course 3"];
 }
